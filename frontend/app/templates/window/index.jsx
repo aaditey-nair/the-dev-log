708db0d6e3170b9cb0d40a5c36f63a99f4768d25
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-04 15:25:24 +0530

    update tailwind.config.js to new project structure

---
+++ frontend/app/templates/window/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Window() {
+  return (
+    <div className="grid">
+      <section className="bg-light">Navbar</section>
+      <section className="bg-accent">Sidebar</section>
+      <section className="bg-accent">Main</section>
+    </div>
+  );
+}
+
+export default Window;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,22 +1,10 @@
 import React, { useState } from "react";
-import axios from "axios";
+import Window from "../app/templates/window";
 
 export default function Home() {
-  const [data, setData] = useState("server is not running");
-
-  const getData = () => {
-    axios.get(`http://localhost:5000/`).then((res) => {
-      const response = res.data;
-      setData(response.response);
-    });
-  };
-
   return (
-    <main className="bg-dark min-h-screen">
-      <h1 className="text-3xl text-primary">{data}</h1>
-      <button className="p-2 bg-secondary" onClick={() => getData()}>
-        Check Server Status
-      </button>
+    <main>
+      <Window />
     </main>
   );
 }
