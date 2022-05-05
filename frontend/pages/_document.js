319d8ecc99cac46da27088885a1f89658d3355cf
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-05 11:24:58 +0530

    fixed 'Warning: Received  for a non-boolean attribute .'

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -6,7 +6,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;900&family=Source+Code+Pro:wght@400;900&display=swap"
             rel="stylesheet"
