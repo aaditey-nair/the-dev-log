478d6c10730f45b98935949e669df4df43dead6f
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-07 10:39:48 +0530

    add scrollbar to main part of blog to prevent overflow

--- frontend/app/templates/window/index.jsx
+++ frontend/app/templates/window/index.jsx
@@ -3,16 +3,18 @@ import Sidebar from "../../sections/Sidebar";
 
 function Window({ children }) {
   return (
-    <div className="grid grid-cols-4 grid-rows-[repeat(16,_minmax(0,_1fr))] min-h-screen">
+    <div className="grid grid-cols-4 grid-rows-[repeat(16,_minmax(0,_1fr))] min-h-screen max-h-screen">
       <section className="bg-accent col-span-full row-span-1 font-sans">
         Navbar
       </section>
       <section className="bg-dark text-light row-span-full row-start-2 font-mono border-r border-t-2 border-stripe">
         <Sidebar />
       </section>
-      <section className="text-light col-span-3 row-span-full row-start-2 col-start-2 border-l border-stripe flex flex-col">
+      <section className="text-light col-span-3 row-span-full row-start-2 col-start-2 border-l border-stripe flex flex-col overflow-hidden">
         <PageHeader />
-        <main className="bg-dark p-4  min-h-max flex-grow">{children}</main>
+        <main className="bg-dark p-4 max-h-full flex-grow overflow-auto">
+          {children}
+        </main>
       </section>
     </div>
   );
