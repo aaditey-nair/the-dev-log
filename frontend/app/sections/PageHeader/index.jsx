5b1922fab43e97fe94179f0ef319e884539953cf
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-07 09:04:56 +0530

    remove breadcrumbs from PageHeader section

--- frontend/app/sections/PageHeader/index.jsx
+++ frontend/app/sections/PageHeader/index.jsx
@@ -10,49 +10,23 @@ import {
 function PageHeader() {
   const router = useRouter();
   return (
-    <div className="font-sans text-dark text-left text-sm space-y-4">
-      <div className="flex items-center gap-4">
-        <div className="py-2 flex items-center px-4 max-w-max rounded-lg bg-primary capitalize">
-          <FontAwesomeIcon size="md" icon={faFile} />
-          {router.asPath === "/" ? (
-            <span className="pl-2">home</span>
-          ) : (
-            <span className="pl-2">
-              {router.asPath.split("/")[router.asPath.split("/").length - 1]}
-            </span>
-          )}
-          <FontAwesomeIcon className="ml-6" size="md" icon={faXmark} />
-        </div>
-        <FontAwesomeIcon
-          size="xs"
-          className="bg-primary p-2 rounded-lg"
-          icon={faPlus}
-        />
-      </div>
-      <div className="bg-secondary max-w-max px-4 gap-2 rounded-lg flex items-center">
-        <span>the-dev-log</span>
+    <div className="flex items-center gap-4 font-sans text-dark text-left text-sm">
+      <div className="py-2 flex items-center px-4 max-w-max rounded-lg bg-primary capitalize">
+        <FontAwesomeIcon size="md" icon={faFile} />
         {router.asPath === "/" ? (
-          <span className="space-x-2">
-            <FontAwesomeIcon icon={faChevronRight} />
-            <span>home</span>
-          </span>
+          <span className="pl-2">home</span>
         ) : (
-          <>
-            {router.asPath.split("/").map((route) => {
-              if (route != "") {
-                return (
-                  <span key={route} className="space-x-2">
-                    <FontAwesomeIcon icon={faChevronRight} />
-                    <span>{route}</span>
-                  </span>
-                );
-              } else {
-                return <></>;
-              }
-            })}
-          </>
+          <span className="pl-2">
+            {router.asPath.split("/")[router.asPath.split("/").length - 1]}
+          </span>
         )}
+        <FontAwesomeIcon className="ml-6" size="md" icon={faXmark} />
       </div>
+      <FontAwesomeIcon
+        size="xs"
+        className="bg-primary p-2 rounded-lg"
+        icon={faPlus}
+      />
     </div>
   );
 }
