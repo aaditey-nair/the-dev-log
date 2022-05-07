babf4eb3c76056a8626df9d4ca08ac1a9cbe097c
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-07 12:28:12 +0530

    make navbar smaller

--- frontend/app/templates/window/index.jsx
+++ frontend/app/templates/window/index.jsx
@@ -3,14 +3,14 @@ import Sidebar from "../../sections/Sidebar";
 
 function Window({ children }) {
   return (
-    <div className="grid grid-cols-4 grid-rows-[repeat(16,_minmax(0,_1fr))] min-h-screen max-h-screen">
-      <section className="bg-accent col-span-full row-span-1 font-sans">
+    <div className="grid grid-cols-4 grid-rows-[repeat(22,_minmax(0,_1fr))] min-h-screen max-h-screen">
+      <section className="bg-accent col-span-full row-span-1 font-sans row-start-1">
         Navbar
       </section>
-      <section className="bg-dark text-light row-span-full row-start-2 font-mono border-r border-t-2 border-stripe">
+      <section className="bg-dark text-light row-span-full font-mono border-r border-t-2 border-stripe row-start-2">
         <Sidebar />
       </section>
-      <section className="text-light col-span-3 row-span-full row-start-2 col-start-2 border-l border-stripe flex flex-col overflow-hidden">
+      <section className="text-light col-span-3 row-span-full col-start-2 border-l border-stripe flex flex-col overflow-hidden row-start-2">
         <PageHeader />
         <main className="bg-dark p-4 max-h-full flex-grow overflow-auto">
           {children}
