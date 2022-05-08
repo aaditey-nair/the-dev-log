1a33c16224a0ff3bf07255787fc4664a0f7f029e
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-08 17:58:47 +0530

    add cursor pointer to CategoryLink

--- frontend/app/elements/CategoryLink/index.jsx
+++ frontend/app/elements/CategoryLink/index.jsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 function CategoryLink({ name }) {
   return (
     <Link href={"/posts?cat=" + name.toLowerCase()}>
-      <span className="bg-dark inline-flex py-2 px-4">{name}</span>
+      <span className="bg-dark cursor-pointer inline-flex py-2 px-4">
+        {name}
+      </span>
     </Link>
   );
 }
