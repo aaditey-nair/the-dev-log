ba47d88488c40943b067ef3fb98c3a406a4e16a5
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-08 14:43:19 +0530

    create slots for home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,87 @@
-export default function Home() {
+import React from "react";
+
+function HomePage() {
   return (
     <>
-      <h1>This is my blog post</h1>
+      <h1 className="text-8xl font-mono text-primary text-center font-black pt-8">
+        dev.log()
+      </h1>
+      <p className="text-center mx-auto my-4 text-light max-w-[80ch]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-secondary h-36 col-span-1"></div>
+        <div className="bg-primary h-36 col-span-2"></div>
+        <div className="bg-light h-36 col-span-2"></div>
+        <div className="bg-secondary h-36 col-span-1"></div>
+        <div className="bg-secondary h-36 col-span-1"></div>
+        <div className="bg-accent h-36 col-span-1"></div>
+        <div className="bg-secondary h-36 col-span-1"></div>
+        <div className="bg-gradient-to-r from-primary to-accent h-36 col-span-full"></div>
+        <div className="col-span-2 space-y-8">
+          <h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </h1>
+          <h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </h1>
+          <h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </h1>
+          <h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </h1>
+          <h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </h1>
+          <h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </h1>
+        </div>
+        <div className="space-y-4">
+          <div className="h-40 bg-secondary"></div>
+          <div className="h-[26rem] bg-secondary"></div>
+        </div>
+      </div>
     </>
   );
 }
+
+export default HomePage;
