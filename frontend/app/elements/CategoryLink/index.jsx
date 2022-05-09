6ccb6554cb5e2cad2446e122f22855615d2c8c93
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-09 10:25:49 +0530

    add hover effect to CategoryLink element

--- frontend/app/elements/CategoryLink/index.jsx
+++ frontend/app/elements/CategoryLink/index.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function CategoryLink({ name }) {
   return (
     <Link href={"/posts?cat=" + name.toLowerCase()}>
-      <span className="bg-dark cursor-pointer inline-flex py-2 px-4">
+      <span className="bg-dark hover:bg-stripe cursor-pointer inline-flex py-2 px-4">
         {name}
       </span>
     </Link>
