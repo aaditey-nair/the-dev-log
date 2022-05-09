4dbcc44d07c593cc9f5f9a282adf421c83809b84
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-09 10:34:15 +0530

    fix(CategoryLink): replace spaces and slashes in URL params

--- frontend/app/elements/CategoryLink/index.jsx
+++ frontend/app/elements/CategoryLink/index.jsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 
 function CategoryLink({ name }) {
+  const href =
+    "/posts?cat=" + name.toLowerCase().replace("/", "_").replace(" ", "-");
   return (
-    <Link href={"/posts?cat=" + name.toLowerCase()}>
+    <Link href={href}>
       <span className="bg-dark hover:bg-stripe cursor-pointer inline-flex py-2 px-4">
         {name}
       </span>
