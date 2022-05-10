5888bef19ab1a334260d5d57b6cbdc184084988f
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-10 14:09:26 +0530

    fix(PageHeader): FontAwesomeIcon invalid size prop value

--- frontend/app/components/PageHeader/index.jsx
+++ frontend/app/components/PageHeader/index.jsx
@@ -7,7 +7,7 @@ function PageHeader() {
   return (
     <div className="bg-accent flex capitalize font-sans text-left text-sm">
       <div className="bg-dark py-2 px-4 text-light flex items-center max-w-max border-t-4 border-primary">
-        <FontAwesomeIcon size="md" icon={faFile} />
+        <FontAwesomeIcon icon={faFile} />
         {router.asPath === "/" ? (
           <span className="pl-2">home</span>
         ) : (
@@ -19,7 +19,7 @@ function PageHeader() {
             }
           </span>
         )}
-        <FontAwesomeIcon className="ml-6" size="md" icon={faXmark} />
+        <FontAwesomeIcon className="ml-6" icon={faXmark} />
       </div>
       <div className="flex-grow border-t-2 border-stripe"></div>
     </div>
