a00255bb4b2a890d0b1c62e88958c883d88d1309
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-11 12:59:57 +0530

    refactor(SubmitDark): change element name

--- frontend/app/elements/SubmitDark/index.jsx
+++ frontend/app/elements/SubmitDark/index.jsx
@@ -1,4 +1,4 @@
-function SUbmitSecondary({ handleSubmit, name }) {
+function SubmitDark({ handleSubmit, name }) {
   return (
     <button
       onClick={(e) => {
@@ -12,4 +12,4 @@ function SUbmitSecondary({ handleSubmit, name }) {
   );
 }
 
-export default SUbmitSecondary;
+export default SubmitDark;
