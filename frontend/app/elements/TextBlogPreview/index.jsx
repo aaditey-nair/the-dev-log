19ac6f4da8c524319412669aca2ecebfa817226b
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-18 16:27:32 +0530

    change(TextBlogPreview): wrap element with article tag for accesibility

--- frontend/app/elements/TextBlogPreview/index.jsx
+++ frontend/app/elements/TextBlogPreview/index.jsx
@@ -2,23 +2,25 @@ import Link from "next/link";
 
 function TextBlogPreview({ details }) {
   return (
-    <Link href={""}>
-      <a className="hover:text-secondary block">
-        <h1 className="text-2xl font-black text-secondary mb-2">
-          Title Goes Here
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <p className="mt-4 text-sm text-primary">Sunday, 8th May 2022</p>
-      </a>
-    </Link>
+    <article>
+      <Link href={""}>
+        <a className="hover:text-secondary block">
+          <h1 className="text-2xl font-black text-secondary mb-2">
+            Title Goes Here
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p className="mt-4 text-sm text-primary">Sunday, 8th May 2022</p>
+        </a>
+      </Link>
+    </article>
   );
 }
 
