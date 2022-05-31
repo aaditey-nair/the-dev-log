2ce5288412d4a05daa17c7679b4d602b3e02421b
Aaditey-Nair <aaditeyn@gmail.com>
2022-05-31 10:38:21 +0530

    change(Window): move navbar from top to bottom and convert it to a status bar

--- app/templates/window/index.jsx
+++ app/templates/window/index.jsx
@@ -5,17 +5,17 @@ import Sidebar from "../../sections/Sidebar";
 function Window({ children }) {
   return (
     <div className="grid grid-cols-4 grid-rows-[repeat(22,_minmax(0,_1fr))] min-h-screen max-h-screen">
-      <section className="bg-accent col-span-full row-span-1 font-sans row-start-1 text-light z-50">
-        <Navbar />
-      </section>
-      <section className="bg-dark text-light row-span-full font-mono border-r border-t-2 border-stripe row-start-2">
+      <section className="bg-dark text-light row-span-full font-mono border-r border-t-2 border-stripe">
         <Sidebar />
       </section>
-      <section className="text-light col-span-3 row-span-full col-start-2 border-l border-stripe flex flex-col overflow-hidden row-start-2">
+      <section className="text-light col-span-3 row-span-full col-start-2 border-l border-stripe flex flex-col overflow-hidden">
         <PageHeader />
         <main className="bg-dark p-4 max-h-full flex-grow overflow-auto">
           {children}
         </main>
+      </section>
+      <section className="bg-accent col-span-full row-span-1 font-sans text-light z-50">
+        <Navbar />
       </section>
     </div>
   );
