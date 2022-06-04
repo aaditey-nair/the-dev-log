1bb861c90d540dc4615d9be1d825ae067dba7271
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-04 15:28:03 +0530

    change(PageHeader): add bg color to current page tab

--- app/components/PageHeader/index.jsx
+++ app/components/PageHeader/index.jsx
@@ -5,20 +5,22 @@ import { faFile, faXmark } from "@fortawesome/free-solid-svg-icons";
 function PageHeader() {
   const router = useRouter();
   return (
-    <div className="bg-dark capitalize font-sans text-left text-sm py-2 px-4 text-light flex items-center max-w-max border-t-4 border-primary">
-      <FontAwesomeIcon icon={faFile} />
-      {router.asPath === "/" ? (
-        <span className="pl-2">home</span>
-      ) : (
-        <span className="pl-2">
-          {
-            router.asPath
-              .split("/")
-              [router.asPath.split("/").length - 1].split("?")[0]
-          }
-        </span>
-      )}
-      <FontAwesomeIcon className="ml-6" icon={faXmark} />
+    <div className="bg-accent">
+      <div className="bg-dark border-t-4 border-primary py-2 px-4 capitalize font-sans text-left text-sm text-light flex items-center max-w-max">
+        <FontAwesomeIcon icon={faFile} />
+        {router.asPath === "/" ? (
+          <span className="pl-2">home</span>
+        ) : (
+          <span className="pl-2">
+            {
+              router.asPath
+                .split("/")
+                [router.asPath.split("/").length - 1].split("?")[0]
+            }
+          </span>
+        )}
+        <FontAwesomeIcon className="ml-6" icon={faXmark} />
+      </div>
     </div>
   );
 }
