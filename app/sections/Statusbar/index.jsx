cc3e751f280d5915e8706a72fa2bdd16cf3b2b53
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-04 13:25:39 +0530

    change(Statusbar): remove links

--- app/sections/Statusbar/index.jsx
+++ app/sections/Statusbar/index.jsx
@@ -1,29 +1,15 @@
 import StatusbarItem from "../../components/StatusbarItem";
 import StatusbarMenuItem from "../../elements/StatusbarMenuItem";
-import {
-  faBriefcase,
-  faCodeBranch,
-  faDove,
-  faHashtag,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
 
 function Statusbar() {
   return (
-    <nav className="flex items-center px-4 h-full gap-4">
-      <span className="font-mono text-primary">dev.log()</span>
-      <StatusbarItem name="Home" link="/" />
-      <StatusbarItem name="Contact" link="/contact" />
-      <StatusbarItem name="About" link="/about" />
-      <StatusbarItem name="Socials">
-        <StatusbarMenuItem
-          href="https://github.com/Aaditey-Nair"
-          icon={faCodeBranch}
-          name="GitHub"
-        />
-        <StatusbarMenuItem href="/" icon={faDove} name="Twitter" />
-        <StatusbarMenuItem href="/" icon={faHashtag} name="Instagram" />
-        <StatusbarMenuItem href="/" icon={faBriefcase} name="Linkedin" />
-      </StatusbarItem>
+    <nav className="flex items-center px-4 py-1 h-full gap-4">
+      <StatusbarMenuItem
+        href="https://github.com/Aaditey-Nair"
+        icon={faCodeBranch}
+        name="Socials"
+      />
     </nav>
   );
 }
