5723be78084efc8c3a2384ac4bf2374beaf769de
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-04 14:16:35 +0530

    change(Statusbar): finish statusbar with user settings and log in info

--- app/sections/Statusbar/index.jsx
+++ app/sections/Statusbar/index.jsx
@@ -6,33 +6,49 @@ import {
   faHashtag,
   faDove,
   faEnvelopesBulk,
+  faCog,
+  faVolumeHigh,
+  faRefresh,
 } from "@fortawesome/free-solid-svg-icons";
 
 function Statusbar() {
   return (
-    <nav className="flex items-center px-4 py-1 h-full gap-4">
-      <StatusbarItem name="socials" icon={faCodeBranch}>
-        <a
-          target="_blank"
-          href="https://github.com/Aaditey-Nair"
-          className="space-x-2 block"
-        >
-          <FontAwesomeIcon size="sm" icon={faCodeCommit} />
-          <span className="text-sm">Github</span>
+    <nav className="px-4 py-1 h-full flex justify-between">
+      <div className="flex items-center gap-4">
+        <StatusbarItem name="socials" icon={faCodeBranch}>
+          <a
+            target="_blank"
+            href="https://github.com/Aaditey-Nair"
+            className="space-x-2 block"
+          >
+            <FontAwesomeIcon size="sm" icon={faCodeCommit} />
+            <span className="text-sm">Github</span>
+          </a>
+          <a target="_blank" href="/" className="space-x-2 block">
+            <FontAwesomeIcon size="sm" icon={faHashtag} />
+            <span className="text-sm">Instagram</span>
+          </a>
+          <a target="_blank" href="/" className="space-x-2 block">
+            <FontAwesomeIcon size="sm" icon={faDove} />
+            <span className="text-sm">Twitter</span>
+          </a>
+          <span className="space-x-2 block">
+            <FontAwesomeIcon size="sm" icon={faEnvelopesBulk} />
+            <span className="text-sm">aaditeyn@gmail.com</span>
+          </span>
+        </StatusbarItem>
+        <a href="/" className="space-x-2 block">
+          <FontAwesomeIcon size="sm" icon={faRefresh} />
         </a>
-        <a target="_blank" href="/" className="space-x-2 block">
-          <FontAwesomeIcon size="sm" icon={faHashtag} />
-          <span className="text-sm">Instagram</span>
-        </a>
-        <a target="_blank" href="/" className="space-x-2 block">
-          <FontAwesomeIcon size="sm" icon={faDove} />
-          <span className="text-sm">Twitter</span>
-        </a>
-        <span className="space-x-2 block">
-          <FontAwesomeIcon size="sm" icon={faEnvelopesBulk} />
-          <span className="text-sm">aaditeyn@gmail.com</span>
-        </span>
-      </StatusbarItem>
+      </div>
+      <div className="flex items-center gap-4 text-sm">
+        <span>Status: Logged Out</span>
+        <a href="/sign-in">Sign In</a>
+        <a href="/sign-in">Log In</a>
+        <a href="/sign-in">Log out</a>
+        <FontAwesomeIcon size="sm" icon={faVolumeHigh} />
+        <FontAwesomeIcon size="sm" icon={faCog} />
+      </div>
     </nav>
   );
 }
