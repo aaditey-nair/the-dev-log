e08f6b7a00150f67521335b8c1ee5e023a2a9863
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-04 13:34:49 +0530

    change(StatusbarMenuItem): make text and icon smaller

--- app/elements/StatusbarMenuItem/index.jsx
+++ app/elements/StatusbarMenuItem/index.jsx
@@ -2,9 +2,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function StatusMenuItem({ name, icon, href }) {
   return (
-    <a target="_blank" href={href} className="space-x-2 block">
-      <FontAwesomeIcon icon={icon} />
-      <span>{name}</span>
+    <a target="_blank" href={href} className="space-x-2">
+      <FontAwesomeIcon size="sm" icon={icon} />
+      <span className="text-sm">{name}</span>
     </a>
   );
 }
