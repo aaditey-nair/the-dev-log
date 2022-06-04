1985ba3493972ec85bc60bf332adf8937178551c
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-04 14:30:35 +0530

    change(PageHeader): remove top border

--- app/components/PageHeader/index.jsx
+++ app/components/PageHeader/index.jsx
@@ -5,23 +5,20 @@ import { faFile, faXmark } from "@fortawesome/free-solid-svg-icons";
 function PageHeader() {
   const router = useRouter();
   return (
-    <div className="bg-accent flex capitalize font-sans text-left text-sm">
-      <div className="bg-dark py-2 px-4 text-light flex items-center max-w-max border-t-4 border-primary">
-        <FontAwesomeIcon icon={faFile} />
-        {router.asPath === "/" ? (
-          <span className="pl-2">home</span>
-        ) : (
-          <span className="pl-2">
-            {
-              router.asPath
-                .split("/")
-                [router.asPath.split("/").length - 1].split("?")[0]
-            }
-          </span>
-        )}
-        <FontAwesomeIcon className="ml-6" icon={faXmark} />
-      </div>
-      <div className="flex-grow border-t-2 border-stripe"></div>
+    <div className="bg-dark capitalize font-sans text-left text-sm py-2 px-4 text-light flex items-center max-w-max border-t-4 border-primary">
+      <FontAwesomeIcon icon={faFile} />
+      {router.asPath === "/" ? (
+        <span className="pl-2">home</span>
+      ) : (
+        <span className="pl-2">
+          {
+            router.asPath
+              .split("/")
+              [router.asPath.split("/").length - 1].split("?")[0]
+          }
+        </span>
+      )}
+      <FontAwesomeIcon className="ml-6" icon={faXmark} />
     </div>
   );
 }
