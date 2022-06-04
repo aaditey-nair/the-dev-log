785458cb1f89f6d82baa9d8d4419b52e11560fe2
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-04 15:06:24 +0530

    change(Window): remove top border from sidebar

--- app/templates/window/index.jsx
+++ app/templates/window/index.jsx
@@ -5,10 +5,10 @@ import Sidebar from "../../sections/Sidebar";
 function Window({ children }) {
   return (
     <div className="grid grid-cols-4 grid-rows-[repeat(22,_minmax(0,_1fr))] min-h-screen max-h-screen">
-      <section className="bg-dark text-light row-span-full font-mono border-r border-t-2 border-stripe">
+      <section className="bg-dark text-light row-span-full font-mono border-r-2 border-stripe">
         <Sidebar />
       </section>
-      <section className="text-light col-span-3 row-span-full col-start-2 border-l border-stripe flex flex-col overflow-hidden">
+      <section className="text-light col-span-3 row-span-full col-start-2 flex flex-col overflow-hidden">
         <PageHeader />
         <main className="bg-dark p-4 max-h-full flex-grow overflow-auto">
           {children}
