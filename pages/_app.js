2207e6cb548849815141d87f6f680fc87515f9bd
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-13 15:37:33 +0530

    change(_App): change layout based on path to ignore admin page

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,18 @@ import "../styles/globals.css";
 import Window from "../app/templates/window";
 import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
+  if (router.pathname.startsWith("/admin")) {
+    return (
+      <>
+        <Head>
+          <title>Dev Admin</title>
+        </Head>
+        <Component {...pageProps} />
+      </>
+    );
+  }
+
   return (
     <>
       <Head>
