79d642575b90c504dae204b8acaaf42e1b96c478
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-18 13:53:36 +0530

    change(AdminPanel): change new post link and add padding around panel

--- app/sections/AdminPanel/index.jsx
+++ app/sections/AdminPanel/index.jsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 function AdminPanel() {
   return (
-    <nav>
-      <Link href="/new-post">
-        <a className="max-w-max bg-primary text-dark py-2 px-6 hover:bg-secondary transition-colors">
+    <nav className="pl-4 pt-6 space-y-2">
+      <Link href="/admin/new-post">
+        <a className="bg-primary text-dark py-2 px-6 hover:bg-secondary transition-colors">
           New Post
         </a>
       </Link>
