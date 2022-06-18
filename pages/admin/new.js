f6a43a6910d109dd3238fb6a0ed8f8ac3fb234d3
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-18 14:56:38 +0530

    change(Admin New): add basic structure for new post

--- pages/admin/new.js
+++ pages/admin/new.js
@@ -1,7 +1,34 @@
+import SubmitPrimary from "../../app/elements/SubmitPrimary";
+
 function New() {
   return (
-    <div>New</div>
-  )
+    <div className="space-y-4">
+      <input
+        className="min-w-full text-dark bg-light px-4 py-2 focus-within:border-0"
+        placeholder="Post Title"
+        type="text"
+        name="post-title"
+      />
+      <input
+        className="min-w-full text-dark bg-light px-4 py-2 focus-within:border-0"
+        placeholder="Post Subtitle"
+        type="text"
+        name="post-subtitle"
+      />
+      <input
+        type="checkbox"
+        value="Published"
+        id="published"
+        name="post-status"
+        className="accent-primary"
+      />
+      <label className="pl-2" for="published">
+        Published
+      </label>
+      <br />
+      <SubmitPrimary name="Create Post" />
+    </div>
+  );
 }
 
-export default New
+export default New;
