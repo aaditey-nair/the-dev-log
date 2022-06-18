db519f442a87bc30658395d983605fdba45165e2
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-18 13:38:40 +0530

    change(Admin layout): add layout code for side panel and main section

--- app/templates/Admin/index.jsx
+++ app/templates/Admin/index.jsx
@@ -1,8 +1,12 @@
 function Admin({ children }) {
   return (
-    <div>
-      Admin
-      {children}
+    <div className="grid grid-cols-10 min-h-screen max-h-screen">
+      <section className="bg-dark text-light col-span-2 font-mono border-r-2 border-stripe"></section>
+      <section className="text-light col-span-full col-start-3 flex flex-col overflow-hidden">
+        <main className="bg-dark p-4 max-h-full flex-grow overflow-auto">
+          {children}
+        </main>
+      </section>
     </div>
   );
 }
