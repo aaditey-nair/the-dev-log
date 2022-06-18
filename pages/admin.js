20ddc527fe9a369950fc825a6d8014b582ecf8ce
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-18 14:00:24 +0530

    remove(AdminPanel): move new post button from AdminPanel to Admin page

--- pages/admin.js
+++ pages/admin.js
@@ -1,5 +1,15 @@
-function admin() {
-  return <div>admin</div>;
+import Link from "next/link";
+
+function Admin() {
+  return (
+    <>
+      <Link href="/admin/new-post">
+        <a className="bg-primary text-dark py-2 px-6 hover:bg-secondary transition-colors">
+          New Post
+        </a>
+      </Link>
+    </>
+  );
 }
 
-export default admin;
+export default Admin;
