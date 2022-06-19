9d65fc9463ae541b93709a64470323d1d7b3f7d0
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-19 12:39:34 +0530

    change(package*.json, tailwind.config.css and MdEditor): add styling to rendered markdown using @tailwindcss/typography

--- app/components/MdEditor/index.jsx
+++ app/components/MdEditor/index.jsx
@@ -12,7 +12,11 @@ function MdEditor({ placeholder }) {
         }}
         className="bg-accent p-4 block"
       ></textarea>
-      <ReactMarkdown className="bg-accent p-4" children={content} />
+      <ReactMarkdown
+        className="p-4 prose prose-headings:font-mono prose-headings:text-primary prose-p:text-light 
+        prose-strong:text-secondary prose-ul:text-light prose-ul:leading-snug prose-ol:text-light prose-ol:leading-snug "
+        children={content}
+      />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,15 @@
+const disabledCss = {
+  "code::before": false,
+  "code::after": false,
+  "blockquote p:first-of-type::before": false,
+  "blockquote p:last-of-type::after": false,
+  pre: false,
+  code: false,
+  "pre code": false,
+  "code::before": false,
+  "code::after": false,
+};
+
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -13,7 +25,15 @@ module.exports = {
       sans: ["Lato", "sans-serif"],
       mono: ["Source Code Pro", "monospace"],
     },
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: { css: disabledCss },
+        sm: { css: disabledCss },
+        lg: { css: disabledCss },
+        xl: { css: disabledCss },
+        "2xl": { css: disabledCss },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
