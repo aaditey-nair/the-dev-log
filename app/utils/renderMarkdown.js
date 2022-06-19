532536b2d9fa3146839e5bbf02823bda342b3227
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-19 13:21:22 +0530

    change(package*.json): add remark-gfm package for tasklists, links and tables

--- app/utils/renderMarkdown.js
+++ app/utils/renderMarkdown.js
@@ -1,11 +1,13 @@
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 function renderMarkdown(content) {
   return (
     <ReactMarkdown
-      className="p-4 prose prose-headings:font-mono prose-headings:text-primary prose-p:text-light 
-    prose-strong:text-secondary prose-ul:text-light prose-ul:leading-snug prose-ol:text-light prose-ol:leading-snug "
+      className="p-4 prose prose-invert prose-headings:font-mono prose-headings:text-primary prose-p:text-light 
+    prose-strong:text-secondary prose-ul:text-light prose-ul:leading-snug prose-ol:text-light prose-ol:leading-snug"
       children={content}
+      remarkPlugins={[remarkGfm]}
     />
   );
 }
