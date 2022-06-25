794e4916e48909ade09c171a8d434e94c8c15afd
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-25 16:12:16 +0530

    change(New): create newPost object to contain all info about post onSubmit

--- pages/admin/new.js
+++ pages/admin/new.js
@@ -1,27 +1,62 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import SubmitPrimary from "../../app/elements/SubmitPrimary";
 import MdEditor from "../../app/components/MdEditor";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function New() {
+  const [postData, setPostData] = useState({
+    data: {
+      title: "",
+      subtitle: "",
+      tags: [],
+      collection: "",
+    },
+  });
   const editor = useRef(null);
   return (
     <div className="grid grid-cols-2 gap-4">
       <input
+        onChange={(e) => {
+          setPostData((prev) => ({
+            data: {
+              ...prev.data,
+              title: e.target.value,
+            },
+          }));
+        }}
         className="min-w-full text-dark bg-light px-4 py-2 focus-within:border-0"
         placeholder="Post Title"
         type="text"
         name="post-title"
       />
       <input
+        onChange={(e) => {
+          setPostData((prev) => ({
+            data: {
+              ...prev.data,
+              subtitle: e.target.value,
+            },
+          }));
+        }}
         className="min-w-full text-dark bg-light px-4 py-2 focus-within:border-0"
         placeholder="Post Subtitle"
         type="text"
         name="post-subtitle"
       />
       <div className="flex items-center">
-        <select className="bg-accent p-2 appearance-none">
+        <select
+          onChange={(e) => {
+            setPostData((prev) => ({
+              data: {
+                ...prev.data,
+                collection:
+                  e.target.value == "Select Collection" ? "" : e.target.value,
+              },
+            }));
+          }}
+          className="bg-accent p-2 appearance-none"
+        >
           <option>Select Collection</option>
           <option>Category 1</option>
           <option>Category 2</option>
@@ -47,16 +82,55 @@ function New() {
       <div className="col-span-full">
         <h1 className="inline text-lg">Tags</h1>
         <div className="flex gap-4 mt-2 text-dark">
-          <div className="px-2 py-1 bg-secondary">Python</div>
-          <div className="px-2 py-1 bg-secondary">AI/ML</div>
-          <div className="px-2 py-1 bg-secondary">Data Science</div>
-          <FontAwesomeIcon icon={faPlus} className="text-dark p-2 bg-primary" />
+          {postData.data.tags.map((tag) => {
+            return (
+              <div
+                onClick={() => {
+                  setPostData((prev) => ({
+                    data: {
+                      ...prev.data,
+                      tags: prev.data.tags.filter(
+                        (filterTag) => filterTag !== tag
+                      ),
+                    },
+                  }));
+                }}
+                className="px-2 py-1 bg-secondary"
+              >
+                {tag}
+              </div>
+            );
+          })}
         </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setPostData((prev) => ({
+              data: {
+                ...prev.data,
+                tags: [...prev.data.tags, e.target[0].value],
+              },
+            }));
+          }}
+          className="space-x-4 mt-4"
+        >
+          <input
+            className="bg-light py-2 px-2 text-dark"
+            placeholder="python"
+            type="text"
+            name="tags"
+          />
+          <button className="bg-primary px-4 py-2 text-dark">Add</button>
+        </form>
       </div>
       <MdEditor ref={editor} placeholder="# New Post" />
       <SubmitPrimary
         handleSubmit={() => {
-          console.log(editor.current.value);
+          const newPost = {
+            ...postData.data,
+            content: editor.current.value,
+          };
+          console.log(newPost);
         }}
         name="Create Post"
       />
