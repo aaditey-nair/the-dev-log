f933db270e62d3f8795feaea4eb3f0b4b84be247
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-25 14:10:22 +0530

    change(MdEditor): add maximum height to editor and preview

--- app/components/MdEditor/index.jsx
+++ app/components/MdEditor/index.jsx
@@ -4,15 +4,15 @@ import { renderMarkdown } from "../../utils";
 function MdEditor({ placeholder }) {
   const [content, setContent] = useState(placeholder);
   return (
-    <div className="col-span-full grid grid-cols-2 gap-4">
+    <div className="col-span-full grid grid-cols-2 max-h-[28rem] gap-4">
       <textarea
         value={content}
         onChange={(e) => {
           setContent(e.target.value);
         }}
-        className="bg-accent p-4 block"
+        className="bg-accent p-4 block overflow-auto"
       ></textarea>
-      {renderMarkdown(content)}
+      <div className="overflow-auto">{renderMarkdown(content)}</div>
     </div>
   );
 }
