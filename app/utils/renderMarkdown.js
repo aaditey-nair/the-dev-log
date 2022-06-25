6674e5acd88da2148de222f0994f32a16c2a72ce
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-25 13:50:36 +0530

    change(package*.json and renderMarkdown): add syntax highlighting

--- app/utils/renderMarkdown.js
+++ app/utils/renderMarkdown.js
@@ -1,14 +1,34 @@
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
+import { createElement, Fragment, useEffect, useState } from "react";
 
-function renderMarkdown(content) {
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypePrism from "@mapbox/rehype-prism";
+import rehypeReact from "rehype-react/lib";
+
+function renderMarkdown(md) {
+  const [content, setContent] = useState(Fragment);
+  useEffect(() => {
+    console.log("convert md");
+    unified()
+      .use(remarkParse)
+      .use(remarkGfm)
+      .use(remarkRehype)
+      .use(rehypePrism)
+      .use(rehypeReact, { createElement, Fragment })
+      .process(md)
+      .then((output) => {
+        setContent(output.result);
+      });
+  }, [md]);
   return (
-    <ReactMarkdown
+    <div
       className="p-4 prose prose-invert prose-headings:font-mono prose-headings:text-primary prose-p:text-light 
-    prose-strong:text-secondary prose-ul:text-light prose-ul:leading-snug prose-ol:text-light prose-ol:leading-snug"
-      children={content}
-      remarkPlugins={[remarkGfm]}
-    />
+  prose-strong:text-secondary prose-ul:text-light prose-ul:leading-snug prose-ol:text-light prose-ol:leading-snug"
+    >
+      {content}
+    </div>
   );
 }
 
