8365ba83d5a0db8f85b884bbc77245012b1189fe
Aaditey-Nair <aaditeyn@gmail.com>
2022-06-26 08:08:10 +0530

    change(Window): make sidebar thinner

--- app/templates/window/index.jsx
+++ app/templates/window/index.jsx
@@ -4,7 +4,7 @@ import Sidebar from "../../sections/Sidebar";
 
 function Window({ children }) {
   return (
-    <div className="grid grid-cols-10 grid-rows-[repeat(22,_minmax(0,_1fr))] min-h-screen max-h-screen">
+    <div className="grid grid-cols-12 grid-rows-[repeat(22,_minmax(0,_1fr))] min-h-screen max-h-screen">
       <section className="bg-dark text-light col-span-2 row-span-full font-mono border-r-2 border-stripe">
         <Sidebar />
       </section>
