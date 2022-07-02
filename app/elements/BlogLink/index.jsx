a5c99eeb9c98341c20c78ee3b22b89a8cac7f861
Aaditey-Nair <aaditeyn@gmail.com>
2022-07-02 15:08:15 +0530

    change(BlogLink): add hover effect

--- app/elements/BlogLink/index.jsx
+++ app/elements/BlogLink/index.jsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 function BlogLink({ href, children }) {
   return (
     <Link href={href}>
-      <a target="_blank">{children[0]}</a>
+      <a className="hover:text-primary" target="_blank">
+        {children[0]}
+      </a>
     </Link>
   );
 }
