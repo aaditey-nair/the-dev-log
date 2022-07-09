42a2a08bd2ba54044b5a1dab0957ee06c24967c4
Aaditey-Nair <aaditeyn@gmail.com>
2022-07-09 17:13:00 +0530

    change(api/post): add method to create post

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -5,6 +5,8 @@ const prisma = new PrismaClient();
 export default async function handler(req, res) {
   if (req.method == "GET") {
     return await getPosts(req, res);
+  } else if (req.method == "POST") {
+    return await makePost(req, res);
   } else {
     return res
       .status(405)
@@ -21,5 +23,25 @@ async function getPosts(req, res) {
     return res
       .status(500)
       .json({ error: "Error getting posts", success: false });
+  }
+}
+
+async function makePost(req, res) {
+  const body = req.body;
+  try {
+    const newBlog = await prisma.blog.create({
+      data: {
+        title: body.title,
+        slug: body.slug,
+        tags: body.tags,
+        path: body.content,
+      },
+    });
+    return res.status(200).json(newBlog, { success: true });
+  } catch (error) {
+    console.log("Request error", error);
+    return res
+      .status(500)
+      .json({ error: "Error creating post", success: false });
   }
 }
