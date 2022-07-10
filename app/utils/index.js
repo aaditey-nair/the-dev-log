c1ba0e9b3ce479ee8cf9fbb05cbea3ac10bccf0c
Aaditey-Nair <aaditeyn@gmail.com>
2022-07-10 17:41:16 +0530

    change(utils): remove import for toKebabCase

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,5 +1,4 @@
 import dataRequired from "./validators/dataRequired";
 import isEmail from "./validators/isEmail";
-import toKebabCase from "./toKebabCase";
 
-export { dataRequired, isEmail, toKebabCase };
+export { dataRequired, isEmail };
