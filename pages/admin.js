a7d55a55cfe0f0769fdb96d5d77bc47a52138d37
Aaditey-Nair <aaditeyn@gmail.com>
2022-07-10 12:54:59 +0530

    change(Admin): add form for new collection

--- pages/admin.js
+++ pages/admin.js
@@ -9,6 +9,12 @@ function Admin() {
           New Post
         </a>
       </Link>
+      <form className="my-2" action="" method="POST">
+        <input className="py-2 px-1 mr-2 text-dark" name="collectionName" />
+        <button className="bg-primary max-w-max text-dark py-2 px-6 hover:bg-secondary transition-colors">
+          New Collection
+        </button>
+      </form>
       <div className="mt-8">
         <h1 className="text-xl font-black mb-4">Unpublished Posts</h1>
         <div className="grid grid-cols-5 gap-4">
