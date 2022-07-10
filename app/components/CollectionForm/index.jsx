5d7d91747865cc3e22a1e341c2b138a40f1089ed
Aaditey-Nair <aaditeyn@gmail.com>
2022-07-10 13:23:09 +0530

    change(CollectionForm): use state to track collection name

--- app/components/CollectionForm/index.jsx
+++ app/components/CollectionForm/index.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 function CollectionForm() {
+  const [collectionName, setCollectionName] = useState("");
   return (
-    <form className="my-2" action="/api/collection" method="POST">
-      <input className="py-2 px-1 mr-2 text-dark" name="collectionName" />
+    <div className="my-2">
+      <input
+        onChange={(e) => setCollectionName(e.target.value)}
+        className="py-2 px-1 mr-2 text-dark"
+        name="collectionName"
+        value={collectionName}
+      />
       <button className="bg-primary max-w-max text-dark py-2 px-6 hover:bg-secondary transition-colors">
         New Collection
       </button>
-    </form>
+    </div>
   );
 }
 
