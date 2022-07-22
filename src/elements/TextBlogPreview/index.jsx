c4161d978abbec8c90844876b420ea04a1fbc4ac
Aaditey-Nair <aaditeyn@gmail.com>
2022-07-22 19:13:50 +0530

    change(TextBlogPreview): add width to slug in preview

--- src/elements/TextBlogPreview/index.jsx
+++ src/elements/TextBlogPreview/index.jsx
@@ -10,7 +10,7 @@ function TextBlogPreview({ details }) {
           <h1 className="text-2xl font-black text-light group-hover:text-secondary">
             {details.title}
           </h1>
-          <p className="mt-3 mb-1">{details.slug}</p>
+          <p className="mt-3 mb-1 w-11/12">{details.slug}</p>
           <p className="text-sm text-primary font-mono">{date}</p>
         </a>
       </Link>
