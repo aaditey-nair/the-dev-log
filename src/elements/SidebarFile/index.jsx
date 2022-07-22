3549e89454cb129351bee3cd3cd29d02b4273980
Aaditey-Nair <aaditeyn@gmail.com>
2022-07-22 18:28:17 +0530

    change(SidebarFile): move tooltip visibility to React state

--- src/elements/SidebarFile/index.jsx
+++ src/elements/SidebarFile/index.jsx
@@ -1,22 +1,34 @@
-import React from "react";
-
+import { useState } from "react";
 import Link from "next/link";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
 
-const SidebarFile = ({ fileName, href }) => (
-  <div className="group relative">
-    <Link href={href}>
-      <a className="block whitespace-nowrap text-ellipsis overflow-hidden">
-        <FontAwesomeIcon className="pr-2" size="lg" icon={faFile} />
-        {fileName}
-      </a>
-    </Link>
-    <span className="opacity-0 group-hover:opacity-100 transition-opacity delay-1000 absolute top-2 text-xs left-8 bg-accent text-primary p-2 whitespace-nowrap">
-      {fileName}
-    </span>
-  </div>
-);
+const SidebarFile = ({ fileName, href }) => {
+  const [tooltipVisibility, setTooltipVisibility] = useState(false);
+  return (
+    <div
+      onMouseOver={() => {
+        setTooltipVisibility(true);
+      }}
+      onMouseOut={() => {
+        setTooltipVisibility(false);
+      }}
+      className="relative"
+    >
+      <Link href={href}>
+        <a className="block whitespace-nowrap text-ellipsis overflow-hidden">
+          <FontAwesomeIcon className="pr-2" size="lg" icon={faFile} />
+          {fileName}
+        </a>
+      </Link>
+      {tooltipVisibility && (
+        <span className="absolute -bottom-8 text-xs left-8 bg-accent text-primary p-2 whitespace-nowrap z-50">
+          {fileName}
+        </span>
+      )}
+    </div>
+  );
+};
 
 export default SidebarFile;
