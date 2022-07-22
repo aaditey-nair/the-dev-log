d61e28cc9be53ad7e1b580419255153c5a0d69fb
Aaditey-Nair <aaditeyn@gmail.com>
2022-07-22 18:45:47 +0530

    change(TextBlogPreview): replace dummy data with props

--- src/elements/TextBlogPreview/index.jsx
+++ src/elements/TextBlogPreview/index.jsx
@@ -3,21 +3,13 @@ import Link from "next/link";
 function TextBlogPreview({ details }) {
   return (
     <article>
-      <Link href={""}>
+      <Link href={"/posts/" + details.title}>
         <a className="hover:text-secondary block">
           <h1 className="text-2xl font-black text-secondary mb-2">
-            Title Goes Here
+            {details.title}
           </h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p className="mt-4 text-sm text-primary">Sunday, 8th May 2022</p>
+          <p>{details.slug}</p>
+          <p className="mt-4 text-sm text-primary">{details.createdAt}</p>
         </a>
       </Link>
     </article>
