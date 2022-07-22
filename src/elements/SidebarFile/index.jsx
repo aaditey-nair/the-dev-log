c1df00e59e91052a4fd0d149dd1e32220536f1ae
Aaditey-Nair <aaditeyn@gmail.com>
2022-07-22 18:18:25 +0530

    change(SidebarFile): add tooltip to posts

--- src/elements/SidebarFile/index.jsx
+++ src/elements/SidebarFile/index.jsx
@@ -5,13 +5,18 @@ import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
 
-const SidebarFile = React.memo(({ fileName, href }) => (
-  <Link href={href}>
-    <a className="block whitespace-nowrap text-ellipsis overflow-hidden">
-      <FontAwesomeIcon className="pr-2" size="lg" icon={faFile} />
+const SidebarFile = ({ fileName, href }) => (
+  <div className="group relative">
+    <Link href={href}>
+      <a className="block whitespace-nowrap text-ellipsis overflow-hidden">
+        <FontAwesomeIcon className="pr-2" size="lg" icon={faFile} />
+        {fileName}
+      </a>
+    </Link>
+    <span className="opacity-0 group-hover:opacity-100 transition-opacity delay-1000 absolute top-2 text-xs left-8 bg-accent text-primary p-2 whitespace-nowrap">
       {fileName}
-    </a>
-  </Link>
-));
+    </span>
+  </div>
+);
 
 export default SidebarFile;
