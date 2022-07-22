2042897bc1d80edcf0302a135c413509582b85c6
Aaditey-Nair <aaditeyn@gmail.com>
2022-07-22 19:31:21 +0530

    change(TextBlogPreview): add blog tags

--- src/elements/TextBlogPreview/index.jsx
+++ src/elements/TextBlogPreview/index.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import CategoryLink from "../CategoryLink";
+
 import { formatDate } from "../../utils";
 
 function TextBlogPreview({ details }) {
@@ -7,10 +9,17 @@ function TextBlogPreview({ details }) {
     <article>
       <Link href={"/posts/" + details.title}>
         <a className="group block">
-          <h1 className="text-2xl font-black text-light group-hover:text-secondary">
+          <h1 className="text-2xl font-black text-light group-hover:text-secondary mb-1">
             {details.title}
           </h1>
-          <p className="mt-3 mb-1 w-11/12">{details.slug}</p>
+          {details.tags && (
+            <div className="flex flex-wrap gap-2 my-2.5">
+              {details.tags.map((tag) => {
+                return <CategoryLink name={tag} />;
+              })}
+            </div>
+          )}
+          <p className="mb-1 w-11/12">{details.slug}</p>
           <p className="text-sm text-primary font-mono">{date}</p>
         </a>
       </Link>
