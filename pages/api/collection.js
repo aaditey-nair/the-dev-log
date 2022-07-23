ee267812987d7270a26152fd1abd2f72f7a67854
Aaditey-Nair <aaditeyn@gmail.com>
2022-07-23 11:58:16 +0530

    change(api/collection): add handler for GET request.

--- pages/api/collection.js
+++ pages/api/collection.js
@@ -5,6 +5,8 @@ const prisma = new PrismaClient();
 export default async function handler(req, res) {
   if (req.method === "POST") {
     return await createCollection(req, res);
+  } else if (req.method === "GET") {
+    return await getAllCollections(req, res);
   } else {
     return res
       .status(405)
@@ -26,5 +28,22 @@ async function createCollection(req, res) {
     return res
       .status(500)
       .json({ error: "An error occured while creating the collection" });
+  }
+}
+
+async function getAllCollections(req, res) {
+  try {
+    const collections = await prisma.collection.findMany({
+      select: {
+        name: true,
+        posts: req.query.posts === "true",
+      },
+    });
+    return res.status(200).json(collections, { success: true });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ error: "An error occured while getting the collections" });
   }
 }
