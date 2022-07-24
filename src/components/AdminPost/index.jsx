1792a905cd61401493d2b094c5c48a68a74a2fea
Aaditey-Nair <aaditeyn@gmail.com>
2022-07-24 13:35:46 +0530

    change: AdminPost Make edit icon point to admin/edit/NAME

--- src/components/AdminPost/index.jsx
+++ src/components/AdminPost/index.jsx
@@ -1,13 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
+import Link from "next/link";
 
-function AdminPost({ name, id }) {
+function AdminPost({ name }) {
   return (
     <div className="bg-stripe p-4">
       <h1 className="text-light font-black font-mono mb-4">{name}</h1>
       <div className="flex justify-between">
-        <FontAwesomeIcon className="text-primary" icon={faPenToSquare} />
+        <Link href={"/admin/edit/" + name}>
+          <a>
+            <FontAwesomeIcon className="text-primary" icon={faPenToSquare} />
+          </a>
+        </Link>
         <FontAwesomeIcon
           onClick={() => {
             axios.delete("/api/posts/" + name);
